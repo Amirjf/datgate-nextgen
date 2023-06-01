10e48d919fa7b1a26b4ba88440268a161b534b6f
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2023-06-01 18:19:09 +0330

    fix next config

--- next.config.js
+++ next.config.js
@@ -19,10 +19,10 @@ const nextConfig = {
   },
 };
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
 
-module.exports = withBundleAnalyzer({});
+// module.exports = withBundleAnalyzer({});
 
-// module.exports = nextConfig;
+module.exports = nextConfig;
