bdf18e644bda48bac49c90d02b8111c3e91e62e2
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2023-06-02 15:56:48 +0330

    bring back serverside

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -3,14 +3,11 @@ import { VehiclesList } from 'components/VehiclesList/VehiclesList';
 import ShopSidebar from 'components/shop-sidebar/ShopSidebar';
 import { InventoryContext } from 'contexts/shop/InventoryContext';
 import ShopLayout from 'layouts/shop';
-import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import queryString from 'query-string';
 
 import { fetchInventory } from 'queries/fetchInventory';
 import React, { useContext } from 'react';
-import { useSsr } from 'usehooks-ts';
 
 export const convertArrayToObject = (array: any) => {
   return array.reduce((acc: any, item: any) => {
@@ -32,27 +29,25 @@ export const convertArrayToObject = (array: any) => {
 };
 
 const Inventory = ({ filterData }: any) => {
-  const { addFilter, updateState }: any = useContext(InventoryContext);
-  const { isBrowser } = useSsr();
+  const { addFilter }: any = useContext(InventoryContext);
+  // const { isBrowser } = useSsr();
 
-  const params = isBrowser ? window.location.search : '';
+  // const params = isBrowser ? window.location.search : '';
 
-  const encodedFilters = queryString.parse(params, {
-    arrayFormat: 'comma',
-    parseNumbers: true,
-    parseBooleans: true,
-    arrayFormatSeparator: ',',
-  });
+  // const encodedFilters = queryString.parse(params, {
+  //   arrayFormat: 'comma',
+  //   parseNumbers: true,
+  //   parseBooleans: true,
+  //   arrayFormatSeparator: ',',
+  // });
 
-  for (const key in encodedFilters) {
-    if (!Array.isArray(encodedFilters[key])) {
-      //@ts-ignore
-      encodedFilters[key] = [encodedFilters[key]];
-    }
-  }
+  // for (const key in encodedFilters) {
+  //   if (!Array.isArray(encodedFilters[key])) {
+  //     encodedFilters[key] = [encodedFilters[key]];
+  //   }
+  // }
 
   React.useEffect(() => {
-    updateState(encodedFilters);
     filterData?.selected_filters.map((filter: any) => {
       addFilter({
         key: filter.name,
@@ -73,15 +68,15 @@ const Inventory = ({ filterData }: any) => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   // context.res.setHeader(
   //   'Cache-Control',
   //   'public, s-maxage=10, stale-while-revalidate=59'
   // );
-  //@ts-ignore
-  const getUrl = context.params.slug.join('/');
+
+  // const getUrl = context.params.slug.join('/');
   const queryClient = new QueryClient();
-  await fetchInventory(queryClient, `/${getUrl}`);
+  await fetchInventory(queryClient, context.resolvedUrl);
   return {
     props: {
       dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
@@ -91,28 +86,28 @@ export const getStaticProps: GetStaticProps = async (context) => {
   };
 };
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [
-      {
-        params: {
-          slug: ['new-vehicles'],
-        },
-      },
-      {
-        params: {
-          slug: ['used-vehicles'],
-        },
-      },
-      {
-        params: {
-          slug: ['used-vehicles/certified'],
-        },
-      },
-    ],
-    fallback: true,
-  };
-};
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   return {
+//     paths: [
+//       {
+//         params: {
+//           slug: ['new-vehicles'],
+//         },
+//       },
+//       {
+//         params: {
+//           slug: ['used-vehicles'],
+//         },
+//       },
+//       {
+//         params: {
+//           slug: ['used-vehicles/certified'],
+//         },
+//       },
+//     ],
+//     fallback: true,
+//   };
+// };
 
 Inventory.PageLayout = ShopLayout;
 
